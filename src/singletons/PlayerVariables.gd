99404e6f15a583a858a366bd4af43ed6ee66049b
Nico Schulten <nico@schulten-online.de>
2023-02-04 18:52:47 +0100

    Added move teaching

--- src/singletons/PlayerVariables.gd
+++ src/singletons/PlayerVariables.gd
@@ -4,6 +4,9 @@ var experience = 0
 var health = 1
 var size = 0
 
-enum MOVE { DASH_MOVE, LEAF_BLAST }
+enum MOVE { DASH_MOVE, LEAF_BLAST, ROOT_SLASH }
 
-var learnedMoves = [MOVE.DASH_MOVE]
+var learnedMoves = []
+
+func learnMove(move):
+	learnedMoves.push_back(move)
--- src/transitions/GameOver.gd
+++ src/transitions/GameOver.gd
@@ -2,8 +2,22 @@ extends Node2D
 
 class_name GameOverScreen
 
+const MOVE = PlayerVariables.MOVE
+
 func _on_Button_pressed():
 	Global.changeScene("res://src/levels/Level0.tscn")
 
 func _on_NextLevelButton_pressed():
 	Global.changeScene("res://src/levels/Level1.tscn")
+
+func _on_DashBTN_pressed():
+	if !PlayerVariables.learnedMoves.has(MOVE.DASH_MOVE):
+		PlayerVariables.learnMove(MOVE.DASH_MOVE)
+
+func _on_LeafBlastBTN_pressed():
+	if !PlayerVariables.learnedMoves.has(MOVE.LEAF_BLAST):
+		PlayerVariables.learnMove(MOVE.LEAF_BLAST)
+
+func _on_RootStrikeBTN_pressed():
+	if !PlayerVariables.learnedMoves.has(MOVE.ROOT_SLASH):
+		PlayerVariables.learnMove(MOVE.ROOT_SLASH)
