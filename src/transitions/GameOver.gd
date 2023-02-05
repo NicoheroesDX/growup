5d6c1d66537a71c076cc262b30bd080ec5f72ba7
Nico Schulten <nico@schulten-online.de>
2023-02-05 13:30:45 +0100

    Made abilites learnable

--- src/transitions/GameOver.gd
+++ src/transitions/GameOver.gd
@@ -39,28 +39,7 @@ onready var labelSpeed3 = get_node("AbilitySpeed3/LabelSpeed3")
 
 onready var deathSound = get_node("DeathSound")
 
-func _ready():
-	if PlayerVariables.learnedMoves.has(MOVE.DASH_MOVE):
-		buttonAbilityDash.disabled = true;
-	if PlayerVariables.learnedMoves.has(MOVE.LEAF_BLAST):
-		buttonAbilityLeaf1.disabled = true;
-	if PlayerVariables.learnedMoves.has(MOVE.DOUBLE_LEAF_BLAST):
-		buttonAbilityLeaf2.disabled = true;
-	if PlayerVariables.learnedMoves.has(MOVE.ROOT_SLASH):
-		buttonAbilityRoot.disabled = true;
-	if PlayerVariables.learnedMoves.has(MOVE.TWO_LIFE):
-		buttonAbilityHeart1.disabled = true;
-	if PlayerVariables.learnedMoves.has(MOVE.THREE_LIFE):
-		buttonAbilityHeart2.disabled = true;
-	if PlayerVariables.learnedMoves.has(MOVE.FIVE_LIFE):
-		buttonAbilityHeart3.disabled = true;
-	if PlayerVariables.learnedMoves.has(MOVE.SPEED_TWO):
-		buttonAbilitySpeed1.disabled = true;
-	if PlayerVariables.learnedMoves.has(MOVE.SPEED_FOUR):
-		buttonAbilitySpeed2.disabled = true;
-	if PlayerVariables.learnedMoves.has(MOVE.SPEED_SIX):
-		buttonAbilitySpeed3.disabled = true;
-	
+func refresh_screen():
 	labelDash.text = "Price: " + str(priceAbilityDash)
 	labelLeaf1.text = "Price: " + str(priceAbilityLeaf1)
 	labelLeaf2.text = "Price: " + str(priceAbilityLeaf2)
@@ -71,6 +50,40 @@ func _ready():
 	labelSpeed1.text = "Price: " + str(priceAbilitySpeed1)
 	labelSpeed2.text = "Price: " + str(priceAbilitySpeed2)
 	labelSpeed3.text = "Price: " + str(priceAbilitySpeed3)
+	
+	if PlayerVariables.learnedMoves.has(MOVE.DASH_MOVE):
+		labelDash.text = "Already learned"
+		buttonAbilityDash.disabled = true;
+	if PlayerVariables.learnedMoves.has(MOVE.LEAF_BLAST):
+		labelLeaf1.text = "Already learned"
+		buttonAbilityLeaf1.disabled = true;
+	if PlayerVariables.learnedMoves.has(MOVE.DOUBLE_LEAF_BLAST):
+		labelLeaf2.text = "Already learned"
+		buttonAbilityLeaf2.disabled = true;
+	if PlayerVariables.learnedMoves.has(MOVE.ROOT_SLASH):
+		labelRoot.text = "Already learned"
+		buttonAbilityRoot.disabled = true;
+	if PlayerVariables.learnedMoves.has(MOVE.TWO_LIFE):
+		labelHeart1.text = "Already learned"
+		buttonAbilityHeart1.disabled = true;
+	if PlayerVariables.learnedMoves.has(MOVE.THREE_LIFE):
+		labelHeart2.text = "Already learned"
+		buttonAbilityHeart2.disabled = true;
+	if PlayerVariables.learnedMoves.has(MOVE.FIVE_LIFE):
+		labelHeart3.text = "Already learned"
+		buttonAbilityHeart3.disabled = true;
+	if PlayerVariables.learnedMoves.has(MOVE.SPEED_TWO):
+		labelSpeed1.text = "Already learned"
+		buttonAbilitySpeed1.disabled = true;
+	if PlayerVariables.learnedMoves.has(MOVE.SPEED_FOUR):
+		labelSpeed2.text = "Already learned"
+		buttonAbilitySpeed2.disabled = true;
+	if PlayerVariables.learnedMoves.has(MOVE.SPEED_SIX):
+		labelSpeed3.text = "Already learned"
+		buttonAbilitySpeed3.disabled = true;
+
+func _ready():
+	refresh_screen()
 
 func _on_Button_pressed():
 	Global.changeScene("res://src/levels/Level0.tscn")
@@ -82,48 +95,58 @@ func _on_AbilityDash_pressed():
 	if !PlayerVariables.learnedMoves.has(MOVE.DASH_MOVE):
 		PlayerVariables.learnMove(MOVE.DASH_MOVE)
 		buttonAbilityDash.disabled = true;
+	refresh_screen()
 
 func _on_AbilityLeaf1_pressed():
 	if !PlayerVariables.learnedMoves.has(MOVE.LEAF_BLAST):
 		PlayerVariables.learnMove(MOVE.LEAF_BLAST)
 		buttonAbilityLeaf1.disabled = true;
+	refresh_screen()
 
 func _on_AbilityLeaf2_pressed():
 	if !PlayerVariables.learnedMoves.has(MOVE.DOUBLE_LEAF_BLAST):
 		PlayerVariables.learnMove(MOVE.DOUBLE_LEAF_BLAST)
 		buttonAbilityLeaf2.disabled = true;
+	refresh_screen()
 
 func _on_AbilityRoot_pressed():
 	if !PlayerVariables.learnedMoves.has(MOVE.ROOT_SLASH):
 		PlayerVariables.learnMove(MOVE.ROOT_SLASH)
 		buttonAbilityRoot.disabled = true;
+	refresh_screen()
 
 func _on_AbilityHeart1_pressed():
 	if !PlayerVariables.learnedMoves.has(MOVE.TWO_LIFE):
 		PlayerVariables.learnMove(MOVE.TWO_LIFE)
 		buttonAbilityHeart1.disabled = true;
+	refresh_screen()
 
 func _on_AbilityHeart2_pressed():
 	if !PlayerVariables.learnedMoves.has(MOVE.THREE_LIFE):
 		PlayerVariables.learnMove(MOVE.THREE_LIFE)
 		buttonAbilityHeart2.disabled = true;
+	refresh_screen()
 
 func _on_AbilityHeart3_pressed():
 	if !PlayerVariables.learnedMoves.has(MOVE.FIVE_LIFE):
 		PlayerVariables.learnMove(MOVE.FIVE_LIFE)
 		buttonAbilityHeart3.disabled = true;
+	refresh_screen()
 
 func _on_AbilitySpeed1_pressed():
 	if !PlayerVariables.learnedMoves.has(MOVE.SPEED_TWO):
 		PlayerVariables.learnMove(MOVE.SPEED_TWO)
 		buttonAbilitySpeed1.disabled = true;
+	refresh_screen()
 
 func _on_AbilitySpeed2_pressed():
 	if !PlayerVariables.learnedMoves.has(MOVE.SPEED_FOUR):
 		PlayerVariables.learnMove(MOVE.SPEED_FOUR)
 		buttonAbilitySpeed2.disabled = true;
+	refresh_screen()
 
 func _on_AbilitySpeed3_pressed():
 	if !PlayerVariables.learnedMoves.has(MOVE.SPEED_SIX):
 		PlayerVariables.learnMove(MOVE.SPEED_SIX)
 		buttonAbilitySpeed3.disabled = true;
+	refresh_screen()
